92095a7b141b37c7a20ddaea450bd2b08b001638
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-12-22 00:51:46 +0500

    feat: restructure styles by moving depth and globals CSS to a new directory and updating imports

---
+++ app/pricing/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-white dark:bg-black px-4 sm:px-0">
+      <div className="text-center space-y-6 max-w-2xl z-5 py-16 sm:py-0">
+        {/* <p className="py-1 px-3 bg-zinc-200/30 dark:bg-zinc-900/30 backdrop-blur-sm font-light rounded-full text-zinc-900 dark:text-white inline-block text-sm sm:text-base shadow-m">Join 7,000+ Users</p> */}
+        <div className="space-y-3">
+          <h1 className="text-4xl py-5 sm:text-7xl tracking-tight bg-clip-text bg-linear-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent font-semibold leading-tight">Pricing Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
