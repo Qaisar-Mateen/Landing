33f6847c85504e0cb11f55c733cf87ad09ed8bc7
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-12-22 02:04:22 +0500

    feat: update Home component with new content and add search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-white dark:bg-black px-4 sm:px-0">
       <div className="text-center space-y-6 max-w-2xl z-5 py-16 sm:py-0">
-        <p className="py-1 px-3 bg-zinc-200/30 dark:bg-zinc-900/30 backdrop-blur-sm font-light rounded-full text-zinc-900 dark:text-white inline-block text-sm sm:text-base shadow-m">Join 7,000+ Users</p>
+        <p className="py-1 px-3 bg-zinc-100/30 dark:bg-zinc-900/30 backdrop-blur-sm font-light rounded-full text-zinc-900 dark:text-white inline-block text-sm sm:text-base shadow-m">99% Approval Rate</p>
         <div className="space-y-3">
-          <h1 className="text-4xl sm:text-7xl tracking-tight bg-clip-text bg-linear-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent font-semibold leading-tight">Modern UI Library</h1>
-          <h3 className="text-4xl sm:text-7xl tracking-tight bg-clip-text bg-linear-to-r from-purple-500 via-pink-500 to-blue-500 text-transparent font-semibold leading-tight">For Web Developers</h3>
+          <h1 className="text-4xl pb-2 sm:text-7xl tracking-tight bg-clip-text bg-linear-to-r from-primary via-primary to-blue-300 text-transparent font-semibold leading-tight">Visa Made Simple</h1>
+          {/* <h3 className="text-4xl sm:text-7xl tracking-tight bg-clip-text bg-linear-to-r from-purple-500 via-pink-500 to-blue-500 text-transparent font-semibold leading-tight"></h3> */}
+        </div>
+        <p className="text-gray-400 text-lg text-pretty">
+          Apply online in minutes with a simplified process designed for faster decisions and higher approval rates.
+        </p>
+
+        
+
+        <div className="space-x-3 pt-10">
+          <div className="flex items-center justify-center">
+            <div className="w-full max-w-xl">
+              <label htmlFor="hero-search" className="sr-only">Search</label>
+              <div className="relative shadow-m rounded-full bg-zinc-100/50 dark:bg-zinc-900/50">
+              <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 dark:text-zinc-400" aria-hidden="true" />
+                <input id="hero-search" type="search" placeholder="Search visa types, countries or help" className="w-full pl-12 pr-4 py-3 bg-transparent rounded-lg text-sm placeholder:text-gray-400 outline-none text-zinc-900 dark:text-white" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
